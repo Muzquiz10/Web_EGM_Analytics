d203a6d8915b9396d7de7dfc19fd158470b63efd
Muzquiz10 <muzquiz.10@gmail.com>
2025-12-27 15:48:25 +0100

    boton hamburguesa

--- js/main.js
+++ js/main.js
@@ -29,3 +29,22 @@ form.addEventListener("submit", async (e) => {
   }
 });
 
+// Animación botón hamburguesa
+
+document.addEventListener('DOMContentLoaded', () => {
+const menuToggle = document.querySelector('.menu-toggle');
+const navLinks = document.querySelector('.nav-links');
+
+menuToggle.addEventListener('click', () => {
+  menuToggle.classList.toggle('active');
+  navLinks.classList.toggle('active');
+});
+
+// Cerrar menú al hacer click en un enlace
+document.querySelectorAll('.nav-links a').forEach(link => {
+  link.addEventListener('click', () => {
+    menuToggle.classList.remove('active');
+    navLinks.classList.remove('active');
+  });
+});
+});
